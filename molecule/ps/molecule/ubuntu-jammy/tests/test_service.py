e992f693e2d2a877a5ce4475a8a9ddd88a6b2b71
panchal-yash <panchalyash13@gmail.com>
2025-01-29 12:20:43 +0530

    add test infra for service status check

---
+++ molecule/ps/molecule/ubuntu-jammy/tests/test_service.py
@@ -0,0 +1,9 @@
+import testinfra
+
+def test_service_exists(host):
+    service = host.service("mysql")
+    assert service.exists
+
+def test_service_is_running(host):
+    service = host.service("mysql")
+    assert service.is_running
